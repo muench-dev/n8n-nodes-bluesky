d2f7533ee15a447775e4abda7933fb81d21f8b72
Christian Münch <christian@muench.dev>
2025-06-17 11:57:35 +0200

    chore: update CI configuration to include testing step and adjust test match patterns

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)', '**/*TestNode.node.ts'],
+  testMatch: ['nodes/**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)', 'nodes/**/*TestNode.node.ts'],
   transform: {
     '^.+\\.tsx?$': ['ts-jest', {
       tsconfig: 'tsconfig.json',
